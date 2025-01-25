8b42b90f1a70e052bf659dc378c55f8f697aacd3
Dimitar Dimitrov <dimitrov2k@gmail.com>
2025-01-25 14:35:29 +0200

    Manually use `crossterm` to handle the terminal state in `main`

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
+use std::{io, path::PathBuf};
+
+use crossterm::{
+    cursor, execute,
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
 use tiny_fe::app::{App, ListMode};
 
 fn main() -> anyhow::Result<()> {
-    let mut terminal = ratatui::init();
-    // Clear the terminal and store its current state
-    terminal.clear()?;
+    // Enter the alternate screen and hide the cursor
+    execute!(io::stderr(), EnterAlternateScreen)?;
+    execute!(io::stderr(), cursor::Hide)?;
 
-    let mut app = App::try_new(ListMode::default())?;
-    let app_result = app.run(&mut terminal)?;
+    // Enable raw mode
+    terminal::enable_raw_mode()?;
 
-    // Restore the state of the terminal before the app was opened
-    ratatui::restore();
+    let result = run_app_ui();
 
-    // Return any errors that we've incountered {if any}
-    println!("{}", app_result.to_string_lossy());
+    // Restore the terminal state
+    terminal::disable_raw_mode()?;
+
+    // Leave the alternate screen and show the cursor
+    execute!(io::stderr(), cursor::Show)?;
+    execute!(io::stderr(), LeaveAlternateScreen)?;
+
+    match result {
+        Ok(path) => {
+            println!("{}", path.display());
+        }
+        Err(err) => {
+            eprintln!("Error: {}", err);
+        }
+    }
 
     Ok(())
+}
+
+fn run_app_ui() -> anyhow::Result<PathBuf> {
+    let mut app = App::try_new(ListMode::default())?;
+
+    // Initialize the terminal backend
+    let backend = ratatui::backend::CrosstermBackend::new(io::stderr());
+    let mut terminal = ratatui::Terminal::new(backend)?;
+
+    app.run(&mut terminal)
 }
